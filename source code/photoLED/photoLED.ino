c53a12b2ee69095056ed53c79e76343fee7ce58b
Daniel Jay Bertner <danbertner@gmail.com>
2015-03-12 08:53:23 -0400

    updated code

--- source code/photoLED/photoLED.ino
+++ source code/photoLED/photoLED.ino
@@ -10,7 +10,8 @@ int photocell = A0; //pin our photocell is connected to
 int value; //value of photocell
 
 void setup() {
-  Serial.begin(9600); //serial port speed
+  
+ Serial.begin(9600); //serial port speed
  pinMode(led, OUTPUT); //LED pin is outputting voltage
 
 }
@@ -26,7 +27,6 @@ test this with other sensors (i.e. potentiometer 0-1023,
 ir sensor, ultrasonic sensor, rotary encoder, etc    */
 value = constrain(value, 23, 244); 
 
-
 //print our values from photocell
 Serial.print("photocell value = "  );
 Serial.println( value);
@@ -38,11 +38,9 @@ we are locking out sensor range to a new number range that is
 passed to our "brightness" variable  */
 int brightness = map(value, 23, 244, 0 , 200);
 
-
-
-
 delay(10); //delays entire program 10/1000th millsecond.
-analogWrite(led, brightness);
+
+analogWrite(led, brightness); //applied our brightness range to change with photocell val
 }
 
 
